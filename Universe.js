5492543a52efd382e05292a207f3c527d11eb6af
Mark Hobson <markhobson@gmail.com>
2020-04-17 21:37:54 +0100

    Add background grid for point of reference

--- Universe.js
+++ Universe.js
@@ -11,6 +11,7 @@ function Universe(canvas, bodies) {
 	self.tick = tick;
 	self.resize = resize;
 	self.plot = plot;
+	self.plotGrid = plotGrid;
 	self.tickObjects = tickObjects;
 	self.applyGravity = applyGravity;
 
@@ -48,6 +49,7 @@ function Universe(canvas, bodies) {
 		var origin = centre.sub(bodiesCentre(bodies));
 		this.context.translate(origin.x, origin.y);
 
+		this.plotGrid(-origin.x, -origin.y);
 		this.bodies.forEach(function (body) {
 			body.plot(this.context);
 		}, this);
@@ -55,6 +57,21 @@ function Universe(canvas, bodies) {
 		this.context.restore();
 	}
 
+	function plotGrid(x0, y0) {
+		var canvas = this.context.canvas;
+		var x1 = x0 + canvas.width;
+		var y1 = y0 + canvas.height;
+		var s = 100;
+		var w = 2;
+		this.context.fillStyle = 'white';
+		
+		for (var x = Math.floor(x0 / s) * s; x < Math.ceil(x1 / s) * s; x += s) {
+			for (var y = Math.floor(y0 / s) * s; y < Math.ceil(y1 / s) * s; y += s) {
+				this.context.fillRect(x, y, w, w);
+			}
+		}
+	}
+	
 	function tickObjects(dt) {
 		this.bodies.forEach(function (body) {
 			body.tick(dt);
