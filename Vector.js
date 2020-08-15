44f057511199237fdc2862d4148c5243276959db
Mark Hobson <markhobson@gmail.com>
2020-08-15 12:14:46 +0100

    Dampen camera movement
    Try to reduce motion sickness.

--- Vector.js
+++ Vector.js
@@ -8,6 +8,7 @@ function Vector(x, y) {
 	self.unit = unit;
 	self.mod = mod;
 	self.dot = dot;
+	self.lerp = lerp;
 	
 	function add(v) {
 		return new Vector(this.x + v.x, this.y + v.y);
@@ -32,6 +33,14 @@ function Vector(x, y) {
 	function dot(v) {
 		v = v || this;
 		return this.x * v.x + this.y * v.y;
+	}
+	
+	function lerp(v, t) {
+		return new Vector(lerpScalar(this.x, v.x, t), lerpScalar(this.y, v.y, t));
+	}
+	
+	function lerpScalar(a, b, t) {
+		return (1 - t) * a + t * b;
 	}
 }
 
--- Viewport.js
+++ Viewport.js
@@ -6,10 +6,13 @@ function Viewport(canvas, universe) {
 	var self = this;
 	self.context = canvas.getContext('2d');
 	self.universe = universe;
+	self.position = null;
+	self.speed = 0.01;
 	self.timestamp = null;
 	self.run = run;
 	self.tick = tick;
 	self.plot = plot;
+	self.move = move;
 	self.resize = resize;
 
 	function run() {
@@ -23,8 +26,14 @@ function Viewport(canvas, universe) {
 		dt = Math.min(dt, maxDt);
 		
 		this.resize();
+		if (this.position == null)
+		{
+			this.move(0);
+		}
+		
 		this.plot();
 		this.universe.tick(dt);
+		this.move(dt);
 		
 		this.run();
 	}
@@ -32,15 +41,20 @@ function Viewport(canvas, universe) {
 	function plot() {
 		var canvas = this.context.canvas;
 		this.context.clearRect(0, 0, canvas.width, canvas.height);
-		this.context.save();
-		
-		var centre = new Vector(canvas.width, canvas.height).scale(0.5);
-		var origin = centre.sub(this.universe.centre());
-		this.context.translate(origin.x, origin.y);
+		this.context.save()
+		this.context.translate(this.position.x, this.position.y);
 		
 		this.universe.plot(this.context);
 		
 		this.context.restore();
+	}
+	
+	function move(dt) {
+		var centre = new Vector(canvas.width, canvas.height).scale(0.5);
+		var target = centre.sub(this.universe.centre());
+		
+		var t = Math.min(this.speed * dt, 1);
+		this.position = (this.position || target).lerp(target, t);
 	}
 	
 	function resize() {
